bd356ac19a59ebc53e015761573c072347c16b84
Avishek Thapa <1whitecrush@gmail.com>
2020-01-17 13:06:02 +0545

    first commit'

---
+++ list/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_handles_post_request(self):
+        response = self.client.post('/', {'item_text': 'a list item'})
+        self.assertIn('a list item', response.content.decode())
